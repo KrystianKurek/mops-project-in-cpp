9cea2b4b5a4409af3e3098c1175087cb2829fa9d
Profesorrrr <kszkurek@gmail.com>
2019-05-10 01:17:27 +0200

    MM1

--- main.cpp
+++ main.cpp
@@ -5,15 +5,10 @@
 #include "rand_exp.h"
 #include <fstream>
 using namespace std; 
-template<typename T> void print_queue(T& q) {
-	while (!q.empty()) {
-		std::cout << q.top().time << " ";
-		q.pop();
-	}
-	std::cout << '\n';
-}
+
 int main()
 {
+
 	std::vector<int> sizes{ 1000000 };
 	mops_simulation s(1, 1, 100, sizes, -1, -1, 1000000);
 	s.run();
